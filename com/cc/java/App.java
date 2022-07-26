c58080712d6f09bf02dd24dc2d9ba5bb4e44cdf5
Roman <roman.makhanbetov@gmx.de>
2022-07-26 11:07:01 +0200

    HoneyBee()/Queen()/Drone()/Worker()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,25 @@ public class App {
     
     public static void main(String[] args) {
      
+
+        Worker worker = new Worker();
+        Queen queen = new Queen();
+        Drone drone = new Drone();
+
+        output(worker.doYourJob());
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+
+        output("----------------------------");
+
+        output(worker.fly());
+        output(queen.fly());
+        output(drone.fly());
+
+        HoneyBee bee = new HoneyBee();
+        output(bee.doYourJob());
+
+
     }
 
     public static void output(String outputStr) {
